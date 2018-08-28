671b6b029f25c7925818dfdc01b73d323a250770
Aaron Miller <millerac333@gmail.com>
2018-08-28 16:28:21 -0500

    created random class with numbers and added random numbrer generator to populate list

---
+++ Numbers.cs
@@ -0,0 +1,21 @@
+// Random random = new Random();
+// // Create a list
+// // Populate the list
+// With the resulting List, populate a new List that contains each number squared. For example, if the original list is 2, 5, 3, 15, the final list will be 4, 25, 9, 225.
+
+// Then remove any number that is odd from the list of squared numbers.
+
+using System;
+using System.Collections.Generic;
+class Numbers
+{
+        {
+            // Using the Random class, generate a list of 20 random numbers that are in the range of 1-50.
+            Random randomNumbers = new Random();
+
+    Console.WriteLine("Five random integers between 0 and 50:");
+            for (int number = 0; number <= 19; number++)
+                Console.WriteLine(randomNumbers.Next(51));
+            Console.WriteLine();
+        }
+    }
--- Program.cs
+++ Program.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿// With the resulting List, populate a new List that contains each number squared. For example, if the original list is 2, 5, 3, 15, the final list will be 4, 25, 9, 225.
+
+// Then remove any number that is odd from the list of squared numbers.
+
+using System;
+using System.Collections.Generic;
 
 namespace random
 {
@@ -6,7 +11,19 @@ namespace random
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Using the Random class, generate a list of 20 random numbers that are in the range of 1-50.
+            Random randomNumberGenerator = new Random();
+
+            // // Create a list
+            List<int> randomNumbers = new List<int>();
+
+            // // Populate the list
+            for (int number = 0; number <= 19; number++)
+            {
+                randomNumbers.Add(randomNumberGenerator.Next(1, 51));
+                Console.WriteLine($"{randomNumbers[number]}");
+            }
+
         }
     }
 }
